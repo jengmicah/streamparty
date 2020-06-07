e89a3310703ee3b961dce94ba7a5ab3741ec7ba9
jengmicah <jengmicah@gmail.com>
2020-06-07 13:26:00 -0500

    removed multiple dup user check

--- server/users.js
+++ server/users.js
@@ -10,11 +10,11 @@ const checkUser = ({ name, room }) => {
 }
 
 const addUser = ({ id, name, room }) => {
-    name = name.trim().toLowerCase();
-    room = room.trim().toLowerCase();
-    // Check if username exists in this room already
-    const duplicateUser = users.find((user) => user.room === room && user.name === name);
-    if (duplicateUser) return { error: 'DUPLICATE_USER' };
+    // name = name.trim().toLowerCase();
+    // room = room.trim().toLowerCase();
+    // // Check if username exists in this room already
+    // const duplicateUser = users.find((user) => user.room === room && user.name === name);
+    // if (duplicateUser) return { error: 'DUPLICATE_USER' };
 
     const user = { id, name, room };
     users.push(user);
