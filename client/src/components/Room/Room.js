5df9dad2a30a7c68cada0bce55fe446bee415112
jengmicah <jengmicah@gmail.com>
2020-06-15 20:41:09 -0500

    rm unnecessary styles

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -18,23 +18,7 @@ const Room = ({ location, history, match }) => {
     });
     const [room, setRoom] = useState('');
     const [videoProps, setVideoProps] = useState({
-        queue: [
-            // {
-            //     "video": {
-            //         "id": "Y-JQ-RCyPpQ",
-            //         "title": "Relaxing Bossa Nova & Jazz Music For Study - Smooth Jazz Music - Background Music",
-            //         "url": "https://www.youtube.com/watch?v=Y-JQ-RCyPpQ",
-            //         "upload_date": "1 year ago",
-            //         "thumbnail": "https://i.ytimg.com/vi/Y-JQ-RCyPpQ/hqdefault.jpg?sqp=-oaymwEjCPYBEIoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLCnRdFecEZI55Bfh5oRhOQqs_lm_Q",
-            //         "views": "14.6M views"
-            //     },
-            //     "channel": {
-            //         "username": "Cafe Music BGM channel",
-            //         "url": "https://youtube.com/channel/UCJhjE7wbdYAae1G25m0tHAA",
-            //         "verified": true
-            //     }
-            // }
-        ],
+        queue: [],
         history: [],
         playing: true,
         seekTime: 0,
